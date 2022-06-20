1ee53b592d7c7999f53dd75ba524dceec7487c79
Marcel Soute <m.soute@gmail.com>
2022-06-20 11:05:56 +0200

    revert

---
+++ acme_sh/run.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/with-contenv bashio
+
+ACCOUNT=$(bashio::config 'account')
+SERVER=$(bashio::config 'server')
+DOMAINS=$(bashio::config 'domains')
+KEYFILE=$(bashio::config 'keyfile')
+CERTFILE=$(bashio::config 'certfile')
+DNS_PROVIDER=$(bashio::config 'dns.provider')
+DNS_ENVS=$(bashio::config 'dns.env')
+
+for env in $DNS_ENVS; do
+    export $env
+done
+
+DOMAIN_ARR=()
+for domain in $DOMAINS; do
+    DOMAIN_ARR+=(--domain "$domain")
+done
+
+SERVER_ARG=""
+if [ -n "$SERVER" ]; then
+    SERVER_ARG="--server $SERVER"
+fi
+
+/root/.acme.sh/acme.sh --register-account -m ${ACCOUNT} $SERVER_ARG
+
+/root/.acme.sh/acme.sh --issue "${DOMAIN_ARR[@]}" \
+--dns "$DNS_PROVIDER" \
+$SERVER_ARG
+
+/root/.acme.sh/acme.sh --install-cert "${DOMAIN_ARR[@]}" \
+--fullchain-file "/ssl/${CERTFILE}" \
+--key-file "/ssl/${KEYFILE}" \
+
+tail -f /dev/null
